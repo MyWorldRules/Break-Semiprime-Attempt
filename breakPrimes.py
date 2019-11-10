d3287571f25ba75e599718ae15064b8a1a2af39b
Tarun Ravi <tarunvravi@gmail.com>
2019-11-10 14:42:20 -0500

    Add files via upload

---
+++ breakPrimes.py
@@ -0,0 +1,20 @@
+import sympy
+
+def solve(val):
+    iterations = 0
+    thePrime = sympy.prevprime(val)
+    
+    while not sympy.isprime((int) (val/thePrime)):
+        thePrime = sympy.prevprime(thePrime)
+        iterations+=1
+        if iterations%10000 == 0:
+            print(iterations)
+    return thePrime, (int) (val/thePrime), iterations
+
+print(solve(340282366920938464385711811117245792737))
+
+#print(solve(9))
+
+#Testting
+#print (sympy.isprime(5))
+#print(sympy.prevprime(340282366920938464385711811117245792737))
